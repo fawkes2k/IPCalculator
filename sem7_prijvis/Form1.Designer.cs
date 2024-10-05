0d9a5be06f71b548b5e3a30b2790f72f04b61838
Paweł Łaba <labapawel@gmail.com>
2024-10-05 13:05:46 +0200

    Dodaj pliki projektów.

---
+++ sem7_prijvis/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace sem7_prijvis
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            groupBox2 = new GroupBox();
+            comboBox1 = new ComboBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.AutoSize = true;
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(12, 18);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(341, 67);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Adres IP / Sieci";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(6, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(325, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox2.AutoSize = true;
+            groupBox2.Controls.Add(comboBox1);
+            groupBox2.Location = new Point(12, 78);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(341, 67);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Maska XX lub XXX.XXX.XXX.";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(6, 22);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(325, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(365, 434);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Kalkulator adresów IPv4";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private GroupBox groupBox2;
+        private ComboBox comboBox1;
+    }
+}
