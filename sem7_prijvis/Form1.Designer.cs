56434f81322e67e7a93e4e540222b3b035b03afe
Paweł Łaba <labapawel@gmail.com>
2024-10-05 13:11:38 +0200

    Modyfikacja widoku kalkulatora IP

--- sem7_prijvis/Form1.Designer.cs
+++ sem7_prijvis/Form1.Designer.cs
@@ -32,6 +32,7 @@
             textBox1 = new TextBox();
             groupBox2 = new GroupBox();
             comboBox1 = new ComboBox();
+            groupBox3 = new GroupBox();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             SuspendLayout();
@@ -39,11 +40,10 @@
             // groupBox1
             // 
             groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            groupBox1.AutoSize = true;
             groupBox1.Controls.Add(textBox1);
             groupBox1.Location = new Point(12, 18);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(341, 67);
+            groupBox1.Size = new Size(461, 67);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Adres IP / Sieci";
@@ -53,21 +53,20 @@
             textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             textBox1.Location = new Point(6, 22);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(325, 23);
+            textBox1.Size = new Size(445, 23);
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // groupBox2
             // 
             groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            groupBox2.AutoSize = true;
             groupBox2.Controls.Add(comboBox1);
-            groupBox2.Location = new Point(12, 78);
+            groupBox2.Location = new Point(12, 91);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(341, 67);
+            groupBox2.Size = new Size(461, 67);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
-            groupBox2.Text = "Maska XX lub XXX.XXX.XXX.";
+            groupBox2.Text = "Maska adresu";
             // 
             // comboBox1
             // 
@@ -75,14 +74,25 @@
             comboBox1.FormattingEnabled = true;
             comboBox1.Location = new Point(6, 22);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(325, 23);
+            comboBox1.Size = new Size(445, 23);
             comboBox1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox3.Location = new Point(12, 164);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(461, 293);
+            groupBox3.TabIndex = 1;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Wynik";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(365, 434);
+            ClientSize = new Size(485, 469);
+            Controls.Add(groupBox3);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Name = "Form1";
@@ -92,7 +102,6 @@
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -101,5 +110,6 @@
         private TextBox textBox1;
         private GroupBox groupBox2;
         private ComboBox comboBox1;
+        private GroupBox groupBox3;
     }
 }
